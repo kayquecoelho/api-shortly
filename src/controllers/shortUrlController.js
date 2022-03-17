e7f43a62d30e8d0dfd948e882d2a290f4ae847b0
Kayque <lkcoelho76@gmail.com>
2022-03-17 19:41:12 -0300

    feat: create route to acess a url from database

--- src/controllers/shortUrlController.js
+++ src/controllers/shortUrlController.js
@@ -28,3 +28,27 @@ export async function generateShorten(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getShortUrl(req, res) {
+  const { shortUrl } = req.params;
+
+  try {
+    const result = await connection.query(`
+      SELECT * FROM links WHERE "shortenUrl"=$1
+    `, [shortUrl]);
+
+    if (result.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+
+    const viewsCount = result.rows[0].viewsCount + 1;
+    await connection.query(`
+      UPDATE links SET "viewsCount"=$1 WHERE id=$2
+    `, [viewsCount, result.rows[0].id]);
+
+    return res.send(result.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
--- src/routes/shortUrlRoute.js
+++ src/routes/shortUrlRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { generateShorten } from "../controllers/shortUrlController.js";
+import { generateShorten, getShortUrl } from "../controllers/shortUrlController.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
 const shortUrlRoute = Router();
 
 shortUrlRoute.post("/urls/shorten", validateTokenMiddleware, generateShorten);
+shortUrlRoute.get("/urls/:shortUrl", getShortUrl);
 
 export default shortUrlRoute;
